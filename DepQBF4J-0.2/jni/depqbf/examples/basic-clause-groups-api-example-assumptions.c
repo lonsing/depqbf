1f638ae4d7665f758726bc411fcd736c1e51b413
Florian Lonsing <florian.lonsing@tuwien.ac.at>
2015-02-19 16:40:44 +0100

    added missing files

---
+++ DepQBF4J-0.2/jni/depqbf/examples/basic-clause-groups-api-example-assumptions.c
@@ -0,0 +1,173 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "../qdpll.h"
+
+static void
+print_relevant_assumptions (LitID *assumptions)
+{
+  LitID *p;
+  for (p = assumptions; *p; p++)
+    printf ("%d ", *p);
+  printf ("0");
+}
+
+static unsigned int
+count_relevant_assumptions (LitID *assumptions)
+{
+  unsigned int cnt = 0;
+  LitID *p;
+  for (p = assumptions; *p; p++)
+    cnt++;
+  return cnt;
+}
+
+int main (int argc, char** argv)
+{
+  QDPLL *depqbf = qdpll_create ();
+
+  qdpll_configure (depqbf, "--dep-man=simple");
+  qdpll_configure (depqbf, "--incremental-use");
+
+/* This example is similar to 'basic-clause-groups-api-example.c'. However,
+   instead of using DepQBF's clause group API, we emulate clause groups by
+   augmenting the clauses of the formula with fresh selector variables.
+
+   The use of selector variables is common in incremental SAT and QBF solving.
+
+   ***********************************************************************
+   PLEASE NOTE: the purpose of this example is to demonstrate the difference
+   between incremental solving by selector variables and the clause group and
+   push/pop APIs of DepQBF. 
+   For incremental solving by DepQBF, it is RECOMMENDED to ALWAYS use either
+   the clause group API or the push/pop API. The clause group API is general
+   enough to implement any tasks which can be implemented by selector
+   variables but its use is more comfortable.
+   ***********************************************************************
+
+   Given the following unsatisfiable formula (same as in
+   'basic-clause-groups-api-example.c'):
+
+     p cnf 4 3
+     a 1 2 0
+     e 3 4 0
+     -1 -3 0
+     1 2 4 0
+     1 -4 0
+     
+   To effectively put the variables into groups, we add the variable '5' to
+   the first clause and the variable '6' to the last two clauses. The fresh
+   selector variables 5 and 6 are existentially quantified at the left end
+   of the quantifier prefix.
+
+   Formula with selector variables looks as follows:
+
+    p cnf 6 3
+    e 5 6 0
+    a 1 2 0
+    e 3 4 0
+    5 -1 -3 0
+    6 1 2 4 0
+    6 1 -4 0
+ */
+
+  /* Add existential quantifier block with selector variables 5 and 6. */
+  qdpll_new_scope_at_nesting (depqbf, QDPLL_QTYPE_EXISTS, 1);
+  qdpll_add (depqbf, 5);
+  qdpll_add (depqbf, 6);
+  qdpll_add (depqbf, 0);
+
+  /* Add quantifier blocks and variables of the original formula. */
+  qdpll_new_scope_at_nesting (depqbf, QDPLL_QTYPE_FORALL, 2);
+  qdpll_add (depqbf, 1);
+  qdpll_add (depqbf, 2);
+  qdpll_add (depqbf, 0);
+
+  qdpll_new_scope_at_nesting (depqbf, QDPLL_QTYPE_EXISTS, 3);
+  qdpll_add (depqbf, 3);
+  qdpll_add (depqbf, 4);
+  qdpll_add (depqbf, 0);
+
+  /* Add first clause augmented with selector variable 5. */
+  qdpll_add (depqbf, 5);
+  qdpll_add (depqbf, -1);
+  qdpll_add (depqbf, -3);
+  qdpll_add (depqbf, 0);
+
+  /* Add second and third clause augmented with selector variable 6. */
+  qdpll_add (depqbf, 6);
+  qdpll_add (depqbf, 1);
+  qdpll_add (depqbf, 2);  
+  qdpll_add (depqbf, 4);
+  qdpll_add (depqbf, 0);
+  //---------------------
+  qdpll_add (depqbf, 6);
+  qdpll_add (depqbf, 1);
+  qdpll_add (depqbf, -4);  
+  qdpll_add (depqbf, 0);
+
+  /* By adding the selector variables to the clauses, we have effectively
+     separated the clauses into two separate groups. 
+     In the following, we must assign the selector variables MANUALLY as
+     assumptions through the solver API to enable/disable the desired
+     groups. This must be done before solving the formula by calling
+     'qdpll_sat'. */
+  qdpll_print (depqbf, stdout);
+
+  /* Enable both groups by setting both selector variables to false. */
+  qdpll_assume (depqbf, -5);  
+  qdpll_assume (depqbf, -6);
+
+  /* Formula is expected to be unsatisfiable. */
+  QDPLLResult res = qdpll_sat (depqbf);
+  assert (res == QDPLL_RESULT_UNSAT);
+  printf ("result is %d\n", res);
+  /* Get a list of those selector variables which appear in clauses used by
+     the solver to determine unsatisfiability. */
+  LitID *relevant_assumptions = 
+    qdpll_get_relevant_assumptions (depqbf);
+  qdpll_reset (depqbf);
+  assert (count_relevant_assumptions (relevant_assumptions) == 1);
+  printf ("printing zero-terminated relevant assumptions: ", res);
+  print_relevant_assumptions (relevant_assumptions);
+  printf ("\n", res);
+  /* Caller must free memory of array returned by
+     'qdpll_get_relevant_assumptions'. */
+  free (relevant_assumptions);
+
+  /* Deactivate the group which contains the last two clauses by setting the
+     selector variable to true. This way, these clauses will be permanently
+     satisfied in the fortcoming solver run after calling 'qdpll_sat' and
+     hence are effectively removed from the formula. Note that selector
+     variable 5 has to be set to false again to enable the first clause. */
+  printf ("deactivating group 2 with clauses 1 2 4 0 and 1 -4 0 by assumption 6\n");
+
+  qdpll_assume (depqbf, -5);  
+  qdpll_assume (depqbf, 6);
+
+  qdpll_print (depqbf, stdout);
+
+  /* The formula where the last two clauses are disabled is expected to be
+     satisfiable. */
+  res = qdpll_sat (depqbf);
+  assert (res == QDPLL_RESULT_SAT);
+  printf ("result is %d\n", res);
+  qdpll_reset (depqbf);
+
+  /* By setting the selector variables 5 to true and 6 to false, respectively,
+     we deactivate the first clause and activate the last two, which results in
+     an unsatisfiable formula. */
+  printf ("deactivating group 1 with clause -1 -3 0\n");
+
+  qdpll_assume (depqbf, 5);  
+  qdpll_assume (depqbf, -6);
+
+  qdpll_print (depqbf, stdout);
+
+  res = qdpll_sat (depqbf);
+  assert (res == QDPLL_RESULT_UNSAT);
+  printf ("result is %d\n", res);
+
+  qdpll_delete (depqbf);
+}
---
+++ DepQBF4J-0.2/jni/depqbf/examples/basic-clause-groups-api-example.c
@@ -0,0 +1,168 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "../qdpll.h"
+
+/* Helper function. */
+static void
+print_relevant_clause_groups (ClauseGroupID * clause_group_ids)
+{
+  ClauseGroupID *p;
+  for (p = clause_group_ids; *p; p++)
+    printf ("%d ", *p);
+  printf ("0");
+}
+
+/* Helper function. */
+static unsigned int
+count_relevant_clause_groups (ClauseGroupID * clause_group_ids)
+{
+  unsigned int cnt = 0;
+  ClauseGroupID *p;
+  for (p = clause_group_ids; *p; p++)
+    cnt++;
+  return cnt;
+}
+
+int
+main (int argc, char **argv)
+{
+  QDPLL *depqbf = qdpll_create ();
+
+  qdpll_configure (depqbf, "--dep-man=simple");
+  qdpll_configure (depqbf, "--incremental-use");
+
+  /* Given the following unsatisfiable formula:
+
+     p cnf 4 3
+     a 1 2 0
+     e 3 4 0
+     -1 -3 0
+     1 2 4 0
+     1 -4 0
+
+     The first clause will be put in one clause group and the last two clauses in
+     another group. That last two clauses are unsatisfiable, thus deleting the
+     first clause preserves unsatisfiability.
+   */
+
+  /* Declare outermost universal block with variables 1 and 2. */
+  qdpll_new_scope_at_nesting (depqbf, QDPLL_QTYPE_FORALL, 1);
+  qdpll_add (depqbf, 1);
+  qdpll_add (depqbf, 2);
+  qdpll_add (depqbf, 0);
+
+  /* Declare existential block with variables 3 and 4. */
+  qdpll_new_scope_at_nesting (depqbf, QDPLL_QTYPE_EXISTS, 2);
+  qdpll_add (depqbf, 3);
+  qdpll_add (depqbf, 4);
+  qdpll_add (depqbf, 0);
+
+  /* Create a new clause group with ID 'id1'. The ID of a clause group is used
+     as its handle and can be passed to API functions. */
+  ClauseGroupID id1 = qdpll_new_clause_group (depqbf);
+  /* Newly created clause groups are closed. */
+  assert (!qdpll_get_open_clause_group (depqbf));
+  /* A clause group must be opened before clauses can be added to it. Only one
+     clause group can be open at a time. */
+  qdpll_open_clause_group (depqbf, id1);
+  assert (qdpll_get_open_clause_group (depqbf) == id1);
+  /* Add the clause '-1 -3 0' to the currently open clause group 'id1'. */
+  qdpll_add (depqbf, -1);
+  qdpll_add (depqbf, -3);
+  qdpll_add (depqbf, 0);
+  /* The currently open clause group must be closed before creating or opening
+     another clause group. */
+  qdpll_close_clause_group (depqbf, id1);
+  assert (!qdpll_get_open_clause_group (depqbf));
+
+  /* Create another clause group 'id2'. */
+  ClauseGroupID id2 = qdpll_new_clause_group (depqbf);
+  assert (!qdpll_get_open_clause_group (depqbf));
+  qdpll_open_clause_group (depqbf, id2);
+  assert (qdpll_get_open_clause_group (depqbf) == id2);
+  /* Add the clauses '1 2 4 0' and '1 -4 0' to the currently open clause group
+     'id2'. */
+  qdpll_add (depqbf, 1);
+  qdpll_add (depqbf, 2);
+  qdpll_add (depqbf, 4);
+  qdpll_add (depqbf, 0);
+  //---------------------
+  qdpll_add (depqbf, 1);
+  qdpll_add (depqbf, -4);
+  qdpll_add (depqbf, 0);
+  qdpll_close_clause_group (depqbf, id2);
+  assert (!qdpll_get_open_clause_group (depqbf));
+
+  qdpll_print (depqbf, stdout);
+
+  /* Solve the formula, which is unsatisfiable. */
+  QDPLLResult res = qdpll_sat (depqbf);
+  assert (res == QDPLL_RESULT_UNSAT);
+  printf ("result is %d\n", res);
+
+  /* Get a list of those clause groups which contain clauses used by solver to
+     determine unsatisfiability. This amounts to an unsatisfiable core of the
+     formula. */
+  ClauseGroupID *relevant_clause_groups =
+    qdpll_get_relevant_clause_groups (depqbf);
+  /* We must reset the solver AFTER retrieving the relevant groups. */
+  qdpll_reset (depqbf);
+  /* In our example, the clauses in the group 'id2' are relevant for
+     unsatisfiability. (The clause '-1 -3 0' in 'id1' cannot be part of a resolution
+     refutation found by the solver.) */
+  assert (count_relevant_clause_groups (relevant_clause_groups) == 1);
+  assert (relevant_clause_groups[0] == id2);
+  printf ("printing zero-terminated relevant clause group IDs: ", res);
+  print_relevant_clause_groups (relevant_clause_groups);
+  printf ("\n", res);
+
+  /* Temporarily remove the clause group 'id2' by deactivating it. */
+  printf ("deactivating group 2 with clauses 1 2 4 0 and 1 -4 0\n");
+  qdpll_deactivate_clause_group (depqbf, relevant_clause_groups[0]);
+
+  /* Calling 'qdpll_gc()' removes superfluous variables and
+     quantifiers from the prefix. HOWEVER, in this case, we have
+     deactivated -- not deleted -- group 'id2' and hence calling
+     'qdpll_gc()' has no effect. */
+  qdpll_gc (depqbf);
+  qdpll_print (depqbf, stdout);
+
+  /* The formula where group 'id2' has been deactivated is satisfiable. */
+  res = qdpll_sat (depqbf);
+  assert (res == QDPLL_RESULT_SAT);
+  printf ("result is %d\n", res);
+  qdpll_reset (depqbf);
+  /* Activate group 'id2' again, which makes the formula unsatisfiable. */
+  printf ("activating group 2 again\n");
+  qdpll_activate_clause_group (depqbf, relevant_clause_groups[0]);
+
+  /* Free memory of array returned by 'qdpll_get_relevant_clause_groups'. 
+     This is the caller's responsibility. */
+  free (relevant_clause_groups);
+
+  /* Permanently remove the group 'id1'. This operation cannot be undone and
+     is in contrast to deactivating a group. */
+  printf ("deleting group 1 with clause -1 -3 0\n");
+  qdpll_delete_clause_group (depqbf, id1);
+  /* Deleting a group invalidates its ID, which can be checked by
+     'qdpll_exists_clause_group'. */
+  assert (!qdpll_exists_clause_group (depqbf, id1));
+
+  /* Different from the first call of 'qdpll_gc' above, this time variable 3
+     will be removed from the quantifier prefix. We deleted group 'id1' which
+     contains the only clause where variable 3 occurs. Hence calling 'qdpll_gc'
+     removes variable 3 because it does not occur any more in the formula. */
+  qdpll_gc (depqbf);
+  assert (!qdpll_is_var_declared (depqbf, 3));
+  qdpll_print (depqbf, stdout);
+
+  /* After deleting the group 'id1', the formula consisting only of the
+     clauses in group 'id2' is unsatisfiable. */
+  res = qdpll_sat (depqbf);
+  assert (res == QDPLL_RESULT_UNSAT);
+  printf ("result is %d\n", res);
+
+  qdpll_delete (depqbf);
+}
